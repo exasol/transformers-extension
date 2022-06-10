7012d7ee56dcca010d11180dfdecb87e74815e84
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-06-10 10:52:51 +0200

    Added downloader udf

--- exasol_transformers_extension/udfs/model_downloader_udf.py
+++ exasol_transformers_extension/udfs/model_downloader_udf.py
@@ -1,8 +1,40 @@
+import os
+import tempfile
+import pytorch_transformers
+from pathlib import PurePosixPath
+from exasol_bucketfs_utils_python.bucketfs_factory import BucketFSFactory
 
 
 class ModelDownloader:
-    def __init__(self, exa):
+    def __init__(self, exa, downloader_method=pytorch_transformers.AutoModel):
         self.exa = exa
+        self.downloader_method = downloader_method
 
     def run(self, ctx) -> None:
-        raise NotImplementedError
+        model_name = ctx.model_name
+        bfs_conn = ctx.bfs_conn
+
+        # set model path in buckets
+        model_path = model_name.replace('-', '_')
+
+        # create bucketfs location
+        bfs_conn_obj = self.exa.get_connection(bfs_conn)
+        bucketfs_location = BucketFSFactory().create_bucketfs_location(
+            url=bfs_conn_obj.address,
+            user=bfs_conn_obj.user,
+            pwd=bfs_conn_obj.password)
+
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            # download model into tmp folder
+            self.downloader_method.from_pretrained(
+                model_name, cache_dir=tmpdirname)
+
+            # upload the downloaded model files into bucketfs
+            for tmp_file_name in os.listdir(tmpdirname):
+                tmp_file_path = os.path.join(tmpdirname, tmp_file_name)
+                with open(tmp_file_path, mode='rb') as file:
+                    bucketfs_path = PurePosixPath(model_path, tmp_file_name)
+                    bucketfs_location.upload_fileobj_to_bucketfs(
+                        file, str(bucketfs_path))
+
+        ctx.emit(model_path)
