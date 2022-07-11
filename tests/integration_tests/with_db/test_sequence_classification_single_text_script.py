1c88ab06fb0f8462e7b605c8172c112db886b56e
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-07-11 12:03:02 +0200

    Updated integration tests

--- tests/integration_tests/with_db/test_sequence_classification_single_text_script.py
+++ tests/integration_tests/with_db/test_sequence_classification_single_text_script.py
@@ -2,7 +2,8 @@ from tests.utils.parameters import model_params
 
 
 def test_model_downloader_udf_script(
-        upload_language_container, setup_database,
+        # upload_language_container,
+        setup_database,
         pyexasol_connection, upload_model_to_bucketfs):
 
     bucketfs_conn_name, schema_name = setup_database
@@ -12,8 +13,7 @@ def test_model_downloader_udf_script(
         f"SELECT TE_SEQUENCE_CLASSIFICATION_SINGLE_TEXT_UDF("
         f"'{bucketfs_conn_name}', "
         f"'{model_params.name}', "
-        f"'{model_params.text}') "
-        f"EMITS(LABEL_0 DOUBLE,LABEL_1 DOUBLE)"
+        f"'{model_params.text_data}')"
     ).fetchall()
 
     # assertions
