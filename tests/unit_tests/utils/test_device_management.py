f47d489dc7b9186188401f91823bc07a636f2664
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-08-01 19:55:25 +0200

    Fixed tests

--- tests/unit_tests/utils/test_device_management.py
+++ tests/unit_tests/utils/test_device_management.py
@@ -3,28 +3,22 @@ from unittest.mock import patch, MagicMock
 from exasol_transformers_extension.utils import device_management
 
 
-class MockedTorchLibrary:
-    def __init__(self, is_available: bool):
-        self.is_available = is_available
-
-    def is_available(self):
-        return self.is_available
-
-
-@patch('torch.cuda', MagicMock(return_value=MockedTorchLibrary(True)))
+@patch('torch.cuda.is_available', MagicMock(return_value=MagicMock(True)))
 @pytest.mark.parametrize("device_id, expected",
                          [(None, "cpu"), (0, "cuda:0"), (1, "cuda:1")])
 def test_getting_torch_device_available(device_id, expected):
     device = device_management.get_torch_device(device_id)
     device_name = f"{device.type}:{device.index}" \
         if device.index is not None else device.type
+
     assert device_name == expected
 
 
-@patch('torch.cuda', MagicMock(return_value=MockedTorchLibrary(False)))
+@patch('torch.cuda.is_available', MagicMock(return_value=False))
 @pytest.mark.parametrize("device_id, expected",
                          [(None, "cpu"), (0, "cpu"), (1, "cpu")])
 def test_getting_torch_device_not_available(device_id, expected):
     device = device_management.get_torch_device(device_id)
     device_name = device.type
+
     assert device_name == expected
