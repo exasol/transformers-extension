9c2e4480227f0a7f69efd5579cd4fd88863cf2f9
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-08-01 16:48:14 +0200

    Added not available test

--- tests/unit_tests/utils/test_device_management.py
+++ tests/unit_tests/utils/test_device_management.py
@@ -4,16 +4,27 @@ from exasol_transformers_extension.utils import device_management
 
 
 class MockedTorchLibrary:
-    @staticmethod
-    def is_available():
-        return True
+    def __init__(self, is_available: bool):
+        self.is_available = is_available
+
+    def is_available(self):
+        return self.is_available
 
 
-@patch('torch.cuda', MagicMock(return_value=MockedTorchLibrary))
+@patch('torch.cuda', MagicMock(return_value=MockedTorchLibrary(True)))
 @pytest.mark.parametrize("device_id, expected",
                          [(None, "cpu"), (0, "cuda:0"), (1, "cuda:1")])
-def test_getting_torch_device(device_id, expected):
+def test_getting_torch_device_available(device_id, expected):
     device = device_management.get_torch_device(device_id)
     device_name = f"{device.type}:{device.index}" \
         if device.index is not None else device.type
+    assert device_name == expected
+
+
+@patch('torch.cuda', MagicMock(return_value=MockedTorchLibrary(False)))
+@pytest.mark.parametrize("device_id, expected",
+                         [(None, "cpu"), (0, "cpu"), (1, "cpu")])
+def test_getting_torch_device_not_available(device_id, expected):
+    device = device_management.get_torch_device(device_id)
+    device_name = device.type
     assert device_name == expected
