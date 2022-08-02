46b0434fcd73602e4f3e10b8093982badc66b009
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-08-02 16:13:22 +0200

    Updated udfs and tests

--- exasol_transformers_extension/resources/templates/question_answering_udf.jinja.sql
+++ exasol_transformers_extension/resources/templates/question_answering_udf.jinja.sql
@@ -5,7 +5,7 @@ CREATE OR REPLACE {{ language_alias }} SET SCRIPT "TE_QUESTION_ANSWERING_UDF"(
     model_name VARCHAR(2000000),
     question VARCHAR(2000000),
     context_text VARCHAR(2000000)
-    ORDER BY model_name
+    ORDER BY model_name, bucketfs_conn, sub_dir
 )EMITS (
     bucketfs_conn VARCHAR(2000000),
     sub_dir VARCHAR(2000000),
