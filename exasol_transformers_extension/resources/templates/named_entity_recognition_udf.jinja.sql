72fe35eb7689b930abf35a57632951ada8c3733e
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-08-17 13:13:45 +0200

    Changed output columns order

--- exasol_transformers_extension/resources/templates/named_entity_recognition_udf.jinja.sql
+++ exasol_transformers_extension/resources/templates/named_entity_recognition_udf.jinja.sql
@@ -10,8 +10,8 @@ CREATE OR REPLACE {{ language_alias }} SET SCRIPT "TE_NAMED_ENTITY_RECOGNITION_U
     sub_dir VARCHAR(2000000),
     model_name VARCHAR(2000000),
     text_data VARCHAR(2000000),
-    word VARCHAR(2000000),
     word_index INTEGER,
+    word VARCHAR(2000000),
     entity VARCHAR(2000000),
     score DOUBLE ) AS
 
