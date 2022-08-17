4151ea5884f3536a9a32e433097342279944cb1c
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2022-08-17 16:42:09 +0200

    Added topk parameter to UDF

--- exasol_transformers_extension/udfs/models/question_answering_udf.py
+++ exasol_transformers_extension/udfs/models/question_answering_udf.py
@@ -67,8 +67,12 @@ class QuestionAnswering:
                 self.load_models(model_name)
                 self.last_loaded_model_key = current_model_key
 
-            model_pred_df = self.get_prediction(model_df)
-            result_df_list.append(model_pred_df)
+            unique_params = dataframe_operations.get_unique_values(
+                model_df, 'top_k')
+            for top_k in unique_params:
+                param_based_model_df = model_df[model_df['top_k'] == top_k]
+                pred_df = self.get_prediction(param_based_model_df)
+                result_df_list.append(pred_df)
 
         result_df = pd.concat(result_df_list)
         return result_df
@@ -118,49 +122,62 @@ class QuestionAnswering:
         :return: The dataframe where the model_df is formatted with the
         prediction results
         """
-        preds, labels = self._predict_model(model_df)
-        pred_df = self._prepare_prediction_dataframe(model_df, preds, labels)
+        pred_df_list = self._predict_model(model_df)
+        pred_df = self._prepare_prediction_dataframe(model_df, pred_df_list)
         return pred_df
 
-    def _predict_model(self, model_df: pd.DataFrame) -> \
-            Tuple[List[float], List[str]]:
+    def _predict_model(self, model_df: pd.DataFrame) -> List[pd.DataFrame]:
         """
         Predict the given text list using recently loaded models, return
         probability scores and labels
 
         :param model_df: The dataframe to be predicted
 
-        :return: A tuple containing prediction score list and label list
+        :return: List of dataframes holding prediction results
         """
         questions = list(model_df['question'])
         contexts = list(model_df['context_text'])
+        top_k = int(model_df['top_k'].iloc[0])
         results = self.last_created_pipeline(
-            question=questions, context=contexts)
+            question=questions, context=contexts, top_k=top_k)
 
-        answers = []
-        scores = []
+        # We need to separate the answer to one question from the answers to
+        # multiple questions, such that results of one question could be
+        # - a dict where top_k=1, or
+        # - either a dict or list of dicts where top_k>1
+        # in both cases we need to put the answer(s) in a list to make sure that
+        # the answer(s) is from a single question
+        results = [results] if len(questions) == 1 else results
+
+        columns = ["answer", "score"]
+        results_df_list = []
         for result in results:
-            answers.append(result['answer'])
-            scores.append(result['score'])
+            result_df = pd.DataFrame([result]) if type(result) == dict \
+                else pd.DataFrame(result)
+            results_df_list.append(result_df[columns])
 
-        return scores, answers
+        return results_df_list
 
     @staticmethod
     def _prepare_prediction_dataframe(
-            model_df: pd.DataFrame, scores: List[float], answers: List[str]) \
+            model_df: pd.DataFrame, pred_df_list: List[pd.DataFrame]) \
             -> pd.DataFrame:
         """
         Reformat the dataframe used in prediction, such that each input rows
         has a row for each label and its probability score
 
         :param model_df: Dataframe used in prediction
-        :param scores: List of prediction probabilities
-        :param answers: List of predicted answers
+        :param pred_df_list: List of dataframes holding prediction results
 
         :return: Prepared dataframe including input data and predictions
         """
-        model_df['answer'] = answers
-        model_df['score'] = scores
+        n_topk_results = list(map(lambda x: x.shape[0], pred_df_list))
+        repeated_indexes = model_df.index.repeat(repeats=n_topk_results)
+        model_df = model_df.loc[repeated_indexes].reset_index(drop=True)
+
+        # Concat predictions and model_df
+        pred_df = pd.concat(pred_df_list, axis=0).reset_index(drop=True)
+        model_df = pd.concat([model_df, pred_df], axis=1)
 
         return model_df
 
