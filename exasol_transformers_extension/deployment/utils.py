d4d8a376f65a68c412c79dd733ab93d7a3a3e4f8
“umitbuyuksahin” <ucbuyuksahin@gmail.com>
2023-02-01 21:44:25 +0100

    Corrected release path

--- exasol_transformers_extension/deployment/utils.py
+++ exasol_transformers_extension/deployment/utils.py
@@ -18,7 +18,7 @@ BUCKETFS_PASSWORD_ENVIRONMENT_VARIABLE = f"TE_BUCKETFS_PASSWORD"
 SLC_PARTS_PREFIX_NAME = 'language_container_part_0'
 SLC_FINAL_NAME = "language_container.tar.gz"
 N_SLC_PARTS = 2
-GH_RELEASE_URL = "https://github.com/exasol/transformers-extension/releases/tag"
+GH_RELEASE_URL = "https://github.com/exasol/transformers-extension/releases/download"
 
 
 def get_password(pwd: str, user: str, env_var: str, descr: str) -> str:
@@ -44,7 +44,7 @@ def _download_slc_parts(tmp_dir, version):
     for i in range(N_SLC_PARTS):
         slc_part_name = f"{SLC_PARTS_PREFIX_NAME}" + str(i)
         url = "/".join((GH_RELEASE_URL, version, slc_part_name))
-        response = requests.get(url)
+        response = requests.get(url, stream=True)
         with open(Path(tmp_dir, slc_part_name), 'wb') as f:
             f.write(response.content)
 
@@ -66,3 +66,4 @@ def get_container_file_from_github_release(version):
         _download_slc_parts(tmp_dir, version)
         container_file_path = _concatenate_slc_parts(tmp_dir)
         yield container_file_path
+
