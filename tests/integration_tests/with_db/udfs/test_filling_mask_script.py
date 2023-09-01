df9c0f7d5ec35a00c62fc680d37cf9ca563bf3b0
Torsten Kilias <torsten.kilias@exasol.com>
2023-09-01 08:08:17 +0200

    Add python_rows_to_sql in test_filling_mask_script

--- tests/integration_tests/with_db/udfs/test_filling_mask_script.py
+++ tests/integration_tests/with_db/udfs/test_filling_mask_script.py
@@ -1,4 +1,28 @@
+from typing import Tuple, Any, List
+
 from tests.utils.parameters import model_params
+
+
+def python_value_to_sql(value: Any) -> str:
+    if value is None:
+        return "NULL"
+    if isinstance(value, str):
+        return f"'{value}'"
+    if isinstance(value, int):
+        return str(value)
+    raise TypeError(f"Type {type(value)} of value {value} not supported.")
+
+
+def python_row_to_sql(row: Tuple) -> str:
+    sql_values = [python_value_to_sql(value) for value in row]
+    sql_row_str = ",".join(sql_values)
+    return f"({sql_row_str})"
+
+
+def python_rows_to_sql(rows: List[Tuple]) -> str:
+    sql_rows = [python_row_to_sql(row) for row in rows]
+    sql_rows_str = ",".join(sql_rows)
+    return sql_rows_str
 
 
 def test_filling_mask_script(
@@ -12,6 +36,7 @@ def test_filling_mask_script(
         input_data.append((
             '',
             bucketfs_conn_name,
+            None,
             str(model_params.sub_dir),
             model_params.base_model,
             text_data,
@@ -20,12 +45,13 @@ def test_filling_mask_script(
     query = f"SELECT TE_FILLING_MASK_UDF(" \
             f"t.device_id, " \
             f"t.bucketfs_conn_name, " \
+            f"t.token_conn_name, " \
             f"t.sub_dir, " \
             f"t.model_name, " \
             f"t.text_data," \
             f"t.top_k" \
-            f") FROM (VALUES {str(tuple(input_data))} " \
-            f"AS t(device_id, bucketfs_conn_name, sub_dir, " \
+            f") FROM (VALUES {python_rows_to_sql(input_data)} " \
+            f"AS t(device_id, bucketfs_conn_name, token_conn_name, sub_dir, " \
             f"model_name, text_data, top_k));"
 
     # execute sequence classification UDF
