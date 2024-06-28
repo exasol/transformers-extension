1c9545c3d5b4d84c4b99d6d39fe5471cf9b46c6d
mibe <mikhail.beck@exasol.com>
2024-06-28 12:21:37 +0100

    #244 Added some debugging [CodeBuild]

--- tests/integration_tests/with_db/test_upload_model.py
+++ tests/integration_tests/with_db/test_upload_model.py
@@ -29,6 +29,9 @@ def test_model_upload(upload_params,
                       tmp_path: Path,
                       bucketfs_location: bfs.path.PathLike):
 
+    # Debugging
+    assert pyexasol_connection.execute(f"SELECT CURRENT_SCHEMA;").fetchval() == setup_database[1]
+
     sub_dir = 'sub_dir'
     model_specification = model_params.base_model_specs
     model_name = model_specification.model_name
@@ -78,3 +81,6 @@ def test_model_upload(upload_params,
         assert len(result) == 1 and result[0][-1] is None
     finally:
         postprocessing.cleanup_buckets(bucketfs_location, sub_dir)
+
+        # Debugging
+        assert pyexasol_connection.execute(f"SELECT CURRENT_SCHEMA;").fetchval() == setup_database[1]
