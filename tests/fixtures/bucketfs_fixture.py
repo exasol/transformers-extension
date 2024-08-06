a12d8f8c663983a16b48a4d1323c1739a69aef56
Torsten Kilias <torsten.kilias@exasol.com>
2024-08-07 01:47:08 +0200

    Fix bucketfs_fixture.py [CodeBuild]

--- tests/fixtures/bucketfs_fixture.py
+++ tests/fixtures/bucketfs_fixture.py
@@ -1,8 +1,8 @@
 from __future__ import annotations
 
-import pytest
-from pytest_itde.config import TestConfig
 import exasol.bucketfs as bfs
+import pytest
+import pytest_itde
 
 from exasol_transformers_extension.utils.bucketfs_operations import create_bucketfs_location
 from tests.fixtures.database_connection_fixture import BACKEND_SAAS, BACKEND_ONPREM
@@ -11,15 +11,14 @@ from tests.utils.parameters import bucketfs_params
 
 @pytest.fixture(scope="session")
 def bucketfs_location_onprem(backend,
-                             itde_config: TestConfig) -> bfs.path.PathLike | None:
-
+                             bucketfs_config: pytest_itde.config.BucketFs) -> bfs.path.PathLike | None:
     if backend == BACKEND_ONPREM:
         return create_bucketfs_location(
             path_in_bucket=bucketfs_params.path_in_bucket,
             bucketfs_name=bucketfs_params.name,
-            bucketfs_url=itde_config.bucketfs.url,
-            bucketfs_user=itde_config.bucketfs.username,
-            bucketfs_password=itde_config.bucketfs.password,
+            bucketfs_url=bucketfs_config.url,
+            bucketfs_user=bucketfs_config.username,
+            bucketfs_password=bucketfs_config.password,
             bucket=bucketfs_params.bucket)
     return None
 
@@ -30,7 +29,6 @@ def bucketfs_location_saas(backend,
                            saas_account_id,
                            saas_database_id,
                            saas_token) -> bfs.path.PathLike | None:
-
     if backend == BACKEND_SAAS:
         return create_bucketfs_location(
             path_in_bucket=bucketfs_params.path_in_bucket,
@@ -45,7 +43,6 @@ def bucketfs_location_saas(backend,
 def bucketfs_location(backend,
                       bucketfs_location_onprem,
                       bucketfs_location_saas) -> bfs.path.PathLike:
-
     if backend == BACKEND_ONPREM:
         assert bucketfs_location_onprem is not None
         return bucketfs_location_onprem
