ace3e018796b3d46a61916d38cf547a3fcacaccd
Torsten Kilias <torsten.kilias@exasol.com>
2024-08-07 17:50:28 +0200

    change order of the pytest_plugins in conftest.py  [CodeBuild]

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,14 @@
-
 pytest_plugins = [
     "tests.fixtures.database_connection_fixture",
+    "tests.fixtures.script_deployment_fixture",
+    "tests.fixtures.bucketfs_fixture",
     "tests.fixtures.language_container_fixture",
     "tests.fixtures.setup_database_fixture",
-    "tests.fixtures.bucketfs_fixture",
     "tests.fixtures.model_fixture",
-    "tests.fixtures.script_deployment_fixture"
 ]
 
 _BACKEND_OPTION = '--backend'
+
 
 def pytest_addoption(parser):
     parser.addoption(
