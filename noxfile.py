9495f39dfe41ef4ae1cce518a620512712da30da
mibe <mikhail.beck@exasol.com>
2024-09-20 09:47:40 +0100

    #252 Using the pytest plugins

--- noxfile.py
+++ noxfile.py
@@ -26,7 +26,7 @@ def unit_tests(session):
 def integration_tests(session):
     # We need to use a external database here, because the itde plugin doesn't provide all necessary options to
     # configure the database. See the start_database session.
-    session.run('pytest', '--setup-show', '-s', '--itde-db-version=external', 'tests/integration_tests')
+    session.run('pytest', '--setup-show', '-s', '--backend=all', '--itde-db-version=external', 'tests/integration_tests')
 
 
 @nox.session(python=False)
--- tests/conftest.py
+++ tests/conftest.py
@@ -6,19 +6,3 @@ pytest_plugins = [
     "tests.fixtures.setup_database_fixture",
     "tests.fixtures.model_fixture",
 ]
-
-_BACKEND_OPTION = '--backend'
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        _BACKEND_OPTION,
-        action="append",
-        default=[],
-        help=f"""List of test backends (onprem, saas). By default, the tests will be
-            run on both backends. To select only one of the backends add the
-            argument {_BACKEND_OPTION}=<name-of-the-backend> to the command line. Both
-            backends can be selected like ... {_BACKEND_OPTION}=onprem {_BACKEND_OPTION}=saas,
-            but this is the same as the default.
-            """,
-    )
