84c5fb40327354bfb39adfbfd17639eabe73677e
mibe <mikhail.beck@exasol.com>
2024-09-24 13:16:10 +0100

    #252 Set proper dependencies [CodeBuild]

--- tests/fixtures/script_deployment_fixture.py
+++ tests/fixtures/script_deployment_fixture.py
@@ -1,14 +1,14 @@
 """
 This is rather ugly workaround for the problem with incompatible names of the DB and
-BucketFS parameters, used in different scenarios.
+BucketFS parameters, that are used in different scenarios.
 
-In the ideal world the parameters returned by the backend_aware_database_params and
+In the ideal world, the parameters returned by the backend_aware_database_params and
 backend_aware_bucketfs_params fixtures would be suitable for both creating respectively
 a DB or BFS connection and using them in a command line (or more precisely, simulating
 the command line in the context of tests). Unfortunately, the names and even the meanings
 of some of the parameters in those two scenarios do not match.
 
-At some point we will standardise the names and replace the deploy_params and upload_params
+At some point, we will standardise the names and replace the deploy_params and upload_params
 fixtures with backend_aware_database_params and backend_aware_bucketfs_params.
 """
 from __future__ import annotations
@@ -34,7 +34,6 @@ _upload_param_map = {
     'database_id': 'saas_database_id',
     'pat': 'saas_token'
 }
-
 
 def _parse_bucketfs_url(url: str) -> dict[str, Any]:
     parsed_url = urlparse(url)
