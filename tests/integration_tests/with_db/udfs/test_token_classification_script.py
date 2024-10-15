f3b07a04d6e1b75af0752ab9f821c0564ec48aa0
MarleneKress79789 <marlene.kress@gmx.de>
2024-10-15 13:57:19 +0200

    moved duplicate test content to functions

--- tests/integration_tests/with_db/udfs/test_token_classification_script.py
+++ tests/integration_tests/with_db/udfs/test_token_classification_script.py
@@ -2,13 +2,31 @@ from tests.integration_tests.with_db.udfs.python_rows_to_sql import python_rows_
 from tests.utils.parameters import model_params
 
 
+def setup_common_input_data():
+    aggregation_strategy = "simple"
+    n_rows = 100
+    text_data = 'The database software company Exasol is based in Nuremberg'
+    return aggregation_strategy, n_rows, text_data
+
+def correct_number_of_results(added_columns: int, removed_columns: int,
+                              input_data_row: tuple, result: list, n_rows: int):
+    n_cols_result = len(input_data_row) + (added_columns - removed_columns)
+    return len(result) >= n_rows and len(result[0]) == n_cols_result
+
+def lenient_check_of_output_quality(results: list):
+    acceptable_result_sets = [["Exasol", "ORG"], ["Nuremberg", "LOC"]]
+    number_accepted_results = 0
+
+    for i in range(len(results)):
+        if results[i] in acceptable_result_sets:
+            number_accepted_results += 1
+    assert number_accepted_results > len(results)/1.5
+
 def test_token_classification_script_without_spans(
         setup_database, db_conn, upload_token_classification_model_to_bucketfs):
     bucketfs_conn_name, schema_name = setup_database
-    aggregation_strategy = "simple"
-    n_rows = 100
+    aggregation_strategy, n_rows, text_data = setup_common_input_data()
     input_data = []
-    text_data = 'The database software company Exasol is based in Nuremberg'
     for i in range(n_rows):
         input_data.append((
             '',
@@ -34,29 +52,20 @@ def test_token_classification_script_without_spans(
     result = db_conn.execute(query).fetchall()
     # assertions
     assert result[0][-1] is None
-    added_columns = 6  # start_pos,end_pos,word,entity,score,error_message
-    removed_columns = 1  # device_id
-    n_cols_result = len(input_data[0]) + (added_columns - removed_columns)
-    assert len(result) >= n_rows and len(result[0]) == n_cols_result
+    # added_columns: start_pos,end_pos,word,entity,score,error_message
+    # removed_columns: device_id
+    assert correct_number_of_results(6, 1,
+                                     input_data[0], result, n_rows)
+
     # lenient test for quality of results, will be replaced by deterministic test later
     results = [[result[i][7], result[i][8]] for i in range(len(result))]
+    assert lenient_check_of_output_quality(results)
 
-    acceptable_result_sets = [["Exasol", "ORG"], ["Nuremberg", "LOC"]]
-    number_accepted_results = 0
-
-    for i in range(len(results)):
-        if results[i] in acceptable_result_sets:
-            number_accepted_results += 1
-    assert number_accepted_results > len(result)/1.5
-
-#todo exract duplicate stuff to function?
 def test_token_classification_script_with_span(
         setup_database, db_conn, upload_token_classification_model_to_bucketfs):
     bucketfs_conn_name, schema_name = setup_database
-    aggregation_strategy = "simple"
-    n_rows = 100
+    aggregation_strategy, n_rows, text_data = setup_common_input_data()
     input_data = []
-    text_data = 'The database software company Exasol is based in Nuremberg'
     for i in range(n_rows):
         input_data.append((
             '',
@@ -89,17 +98,10 @@ def test_token_classification_script_with_span(
     result = db_conn.execute(query).fetchall()
     # assertions
     assert result[0][-1] is None
-    added_columns = 7  # entity_covered_text, entity_type, score, entity_docid, entity_char_begin, entity_char_end, error_message
-    removed_columns = 5  # device_id, text_data, text_data_docid, text_data_char_begin, text_data_char_end
-    n_cols_result = len(input_data[0]) + (added_columns - removed_columns)
-    assert len(result) >= n_rows and len(result[0]) == n_cols_result
+    # added_columns: entity_covered_text, entity_type, score, entity_docid, entity_char_begin, entity_char_end, error_message
+    # removed_columns: # device_id, text_data, text_data_docid, text_data_char_begin, text_data_char_end
+    assert correct_number_of_results(7, 5,
+                                     input_data[0], result, n_rows)
     # lenient test for quality of results, will be replaced by deterministic test later
     results = [[result[i][4], result[i][5]] for i in range(len(result))]
-
-    acceptable_result_sets = [["Exasol", "ORG"], ["Nuremberg", "LOC"]]
-    number_accepted_results = 0
-
-    for i in range(len(results)):
-        if results[i] in acceptable_result_sets:
-            number_accepted_results += 1
-    assert number_accepted_results > len(result)/1.5
+    assert lenient_check_of_output_quality(results)
