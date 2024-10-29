e8073017380e488d7a99c72129b6842f676f7347
MarleneKress79789 <marlene.kress@gmx.de>
2024-10-29 14:03:19 +0100

    added spans to zero shot and integration test with db

--- exasol_transformers_extension/resources/templates/with_spans/span_zero_shot_text_classification_udf.jinja.sql
+++ exasol_transformers_extension/resources/templates/with_spans/span_zero_shot_text_classification_udf.jinja.sql
@@ -4,17 +4,21 @@ CREATE OR REPLACE {{ language_alias }} SET SCRIPT "TE_ZERO_SHOT_TEXT_CLASSIFICAT
     sub_dir VARCHAR(2000000),
     model_name VARCHAR(2000000),
     text_data VARCHAR(2000000),
+    text_data_docid INTEGER,
+    text_data_char_begin INTEGER,
+    text_data_char_end INTEGER,
     candidate_labels VARCHAR(2000000)
     ORDER BY {{ ordered_columns | join(" ASC,") }} ASC
 )EMITS (
     bucketfs_conn VARCHAR(2000000),
     sub_dir VARCHAR(2000000),
     model_name VARCHAR(2000000),
-    test_data VARCHAR(2000000),
-    candidate_labels VARCHAR(2000000),
     label VARCHAR(2000000),
     score DOUBLE,
     rank INTEGER,
+    text_data_docid INTEGER,
+    text_data_char_begin INTEGER,
+    text_data_char_end INTEGER,
     error_message VARCHAR(2000000) ) AS
 
 {{ script_content }}
