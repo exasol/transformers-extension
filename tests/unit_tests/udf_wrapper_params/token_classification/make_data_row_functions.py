c9fa1c17ece397a214aae43f86834f9eb36181bb
MarleneKress79789 <marlene.kress@gmx.de>
2024-10-31 14:49:22 +0100

    started changing unit tests to use stadaloneudfmack, and be easier maintainable

---
+++ tests/unit_tests/udf_wrapper_params/token_classification/make_data_row_functions.py
@@ -0,0 +1,55 @@
+# base params
+device_id=None
+bucketfs_conn="bfs_conn1"
+sub_dir="sub_dir"
+model_name="model"
+
+text_data="text"
+text_docid = 1
+text_start = 0
+text_end = 6
+
+agg_strategy_simple="simple"
+
+token="token"
+token_start = 2
+token_end = 4
+entity_type="ENTITY_TYPE"
+score=0.1
+error_msg = None
+
+def make_input_row(device_id=device_id, bucketfs_conn=bucketfs_conn, sub_dir=sub_dir,
+                   model_name=model_name, text_data=text_data, aggregation_strategy=agg_strategy_simple):
+    return [(device_id, bucketfs_conn, sub_dir, model_name,text_data, aggregation_strategy)]
+
+def make_output_row(bucketfs_conn=bucketfs_conn, sub_dir=sub_dir,
+                    model_name=model_name, text_data=text_data, aggregation_strategy=agg_strategy_simple,
+                    start=token_start, end=token_end, word=token,
+                    entity=entity_type,score=score, error_msg = error_msg):
+    return [(bucketfs_conn, sub_dir, model_name,text_data, aggregation_strategy,
+             start,end,word, entity,score, error_msg)]
+
+def make_model_output_for_one_input_row(number_entities:int, entity_group=entity_type, score=score, word=token, start=token_start, end=token_end):
+    model_output_single_entities = {'entity_group': entity_group, 'score': score, 'word': word, 'start': start, 'end': end}
+    return [[model_output_single_entities] * number_entities]
+
+def make_input_row_with_span(device_id=device_id, bucketfs_conn=bucketfs_conn, sub_dir="sub_dir1",
+                             model_name=model_name, text_data=text_data, text_data_docid=text_docid,
+                             text_data_char_begin=text_start, text_data_char_end=text_end,
+                             aggregation_strategy=agg_strategy_simple):
+    return [(device_id, bucketfs_conn, sub_dir, model_name,text_data,text_data_docid,
+             text_data_char_begin,text_data_char_end, aggregation_strategy)]
+
+def make_output_row_with_span(bucketfs_conn=bucketfs_conn, sub_dir=sub_dir,
+                              model_name=model_name, text_data_docid=text_docid, text_data_char_begin=text_start,
+                              text_data_char_end=text_end, aggregation_strategy=agg_strategy_simple,
+                              text_start=text_start, token_start=token_start, token_end =token_end,
+                              entity_covered_text=token, entity_type=entity_type, score=score,
+                              error_msg = error_msg):
+    entity_char_begin = text_start + token_start
+    entity_char_end = text_start + token_end
+    entity_docid = text_data_docid
+    return [(bucketfs_conn, sub_dir, model_name,
+             text_data_docid, text_data_char_begin, text_data_char_end, aggregation_strategy,
+             entity_covered_text, entity_type, score, entity_docid, entity_char_begin, entity_char_end,
+             error_msg)]
--- tests/unit_tests/udfs/test_token_classification.py
+++ tests/unit_tests/udfs/test_token_classification.py
@@ -1,57 +1,36 @@
-from unittest.mock import patch
+from typing import Union, List
+from unittest.mock import patch, MagicMock, create_autospec
 
 import pytest
+import transformers
 from exasol_udf_mock_python.column import Column
 from exasol_udf_mock_python.group import Group
+from exasol_udf_mock_python.mock_context import StandaloneMockContext
 from exasol_udf_mock_python.mock_exa_environment import MockExaEnvironment
 from exasol_udf_mock_python.mock_meta_data import MockMetaData
 from exasol_udf_mock_python.udf_mock_executor import UDFMockExecutor
+from transformers import Pipeline
 
-from tests.unit_tests.udf_wrapper_params.token_classification.error_not_cached_multiple_model_multiple_batch import \
-    ErrorNotCachedMultipleModelMultipleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.error_not_cached_single_model_multiple_batch import \
-    ErrorNotCachedSingleModelMultipleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.error_on_prediction_multiple_model_multiple_batch import \
-    ErrorOnPredictionMultipleModelMultipleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.error_on_prediction_single_model_multiple_batch import \
-    ErrorOnPredictionSingleModelMultipleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_bfsconn_single_subdir_single_model_multiple_batch import \
-    MultipleBucketFSConnSingleSubdirSingleModelNameMultipleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_bfsconn_single_subdir_single_model_single_batch import \
-    MultipleBucketFSConnSingleSubdirSingleModelNameSingleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_model_multiple_batch_complete import \
-    MultipleModelMultipleBatchComplete
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_model_multiple_batch_incomplete import \
-    MultipleModelMultipleBatchIncomplete
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_model_multiple_batch_multiple_models_per_batch import \
-    MultipleModelMultipleBatchMultipleModelsPerBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_model_single_batch_complete import \
-    MultipleModelSingleBatchComplete
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_model_single_batch_incomplete import \
-    MultipleModelSingleBatchIncomplete
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_strategy_single_model_multiple_batch import \
-    MultipleStrategySingleModelNameMultipleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.multiple_strategy_single_model_single_batch import \
-    MultipleStrategySingleModelNameSingleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.single_bfsconn_multiple_subdir_single_model_multiple_batch import \
-    SingleBucketFSConnMultipleSubdirSingleModelNameMultipleBatch
+from exasol_transformers_extension.udfs.models.token_classification_udf import TokenClassificationUDF
+from exasol_transformers_extension.utils.model_factory_protocol import ModelFactoryProtocol
 from tests.unit_tests.udf_wrapper_params.token_classification.single_bfsconn_multiple_subdir_single_model_single_batch import \
     SingleBucketFSConnMultipleSubdirSingleModelNameSingleBatch
-from tests.unit_tests.udf_wrapper_params.token_classification.single_model_multiple_batch_complete import \
-    SingleModelMultipleBatchComplete
-from tests.unit_tests.udf_wrapper_params.token_classification.single_model_multiple_batch_incomplete import \
-    SingleModelMultipleBatchIncomplete
 from tests.unit_tests.udf_wrapper_params.token_classification.single_model_single_batch_complete import \
     SingleModelSingleBatchComplete
 from tests.unit_tests.udf_wrapper_params.token_classification.single_model_single_batch_incomplete import \
     SingleModelSingleBatchIncomplete
+
 from tests.unit_tests.udfs.output_matcher import Output, OutputMatcher
 from tests.utils.mock_bucketfs_location import (fake_bucketfs_location_from_conn_object, fake_local_bucketfs_path)
+from tests.utils.mock_cast import mock_cast
 
+def udf_wrapper_empty():
+    # placeholder to use for MockMetaData creation.
+    pass
 
-def create_mock_metadata_with_span(udf_wrapper):
+def create_mock_metadata_with_span():
     meta = MockMetaData(
-        script_code_wrapper_function=udf_wrapper,
+        script_code_wrapper_function=udf_wrapper_empty,
         input_type="SET",
         input_columns=[
             Column("device_id", int, "INTEGER"),
@@ -69,6 +48,9 @@ def create_mock_metadata_with_span(udf_wrapper):
             Column("bucketfs_conn", str, "VARCHAR(2000000)"),
             Column("sub_dir", str, "VARCHAR(2000000)"),
             Column("model_name", str, "VARCHAR(2000000)"),
+            Column("text_data_docid", int, "INTEGER"),
+            Column("text_data_char_begin", int, "INTEGER"),
+            Column("text_data_char_end", int, "INTEGER"),
             Column("aggregation_strategy", str, "VARCHAR(2000000)"),
             Column("entity_covered_text", str, "VARCHAR(2000000)"),
             Column("entity_type", str, "VARCHAR(2000000)"),
@@ -81,9 +63,9 @@ def create_mock_metadata_with_span(udf_wrapper):
     )
     return meta
 
-def create_mock_metadata(udf_wrapper):
+def create_mock_metadata():
     meta = MockMetaData(
-        script_code_wrapper_function=udf_wrapper,
+        script_code_wrapper_function=udf_wrapper_empty,
         input_type="SET",
         input_columns=[
             Column("device_id", int, "INTEGER"),
@@ -114,92 +96,144 @@ def create_mock_metadata(udf_wrapper):
 @pytest.mark.parametrize("params", [
     SingleModelSingleBatchIncomplete,
     SingleModelSingleBatchComplete,
-    SingleModelMultipleBatchIncomplete,
-    SingleModelMultipleBatchComplete,
-    MultipleModelSingleBatchIncomplete,
-    MultipleModelSingleBatchComplete,
-    MultipleModelMultipleBatchIncomplete,
-    MultipleModelMultipleBatchComplete,
-    MultipleModelMultipleBatchMultipleModelsPerBatch,
+    #SingleModelMultipleBatchIncomplete,
+    #SingleModelMultipleBatchComplete,
+    #MultipleModelSingleBatchIncomplete,
+    #MultipleModelSingleBatchComplete,
+    #MultipleModelMultipleBatchIncomplete,
+    #MultipleModelMultipleBatchComplete,
+    #MultipleModelMultipleBatchMultipleModelsPerBatch,
     SingleBucketFSConnMultipleSubdirSingleModelNameSingleBatch,
-    SingleBucketFSConnMultipleSubdirSingleModelNameMultipleBatch,
-    MultipleBucketFSConnSingleSubdirSingleModelNameSingleBatch,
-    MultipleBucketFSConnSingleSubdirSingleModelNameMultipleBatch,
-    MultipleStrategySingleModelNameSingleBatch,
-    MultipleStrategySingleModelNameMultipleBatch,
-    ErrorNotCachedSingleModelMultipleBatch,
-    ErrorNotCachedMultipleModelMultipleBatch,
-    ErrorOnPredictionMultipleModelMultipleBatch,
-    ErrorOnPredictionSingleModelMultipleBatch
+    #SingleBucketFSConnMultipleSubdirSingleModelNameMultipleBatch,
+    #MultipleBucketFSConnSingleSubdirSingleModelNameSingleBatch,
+    #MultipleBucketFSConnSingleSubdirSingleModelNameMultipleBatch,
+    #MultipleStrategySingleModelNameSingleBatch,
+    #MultipleStrategySingleModelNameMultipleBatch,
+    #ErrorNotCachedSingleModelMultipleBatch,
+    #ErrorNotCachedMultipleModelMultipleBatch,
+    #ErrorOnPredictionMultipleModelMultipleBatch,
+    #ErrorOnPredictionSingleModelMultipleBatch
 ])
+
 @patch('exasol.python_extension_common.connections.bucketfs_location.create_bucketfs_location_from_conn_object')
 @patch('exasol_transformers_extension.utils.bucketfs_operations.get_local_bucketfs_path')
 def test_token_classification_with_span(mock_local_path, mock_create_loc, params):
-
     mock_create_loc.side_effect = fake_bucketfs_location_from_conn_object
     mock_local_path.side_effect = fake_local_bucketfs_path
 
-    executor = UDFMockExecutor()
-    meta = create_mock_metadata_with_span(params.work_with_span_udf_wrapper)
+    mock_meta = create_mock_metadata_with_span()
+    input = params.work_with_span_input_data
+    mock_ctx = StandaloneMockContext(inp=input, metadata=mock_meta)
 
-    exa = MockExaEnvironment(
-        metadata=meta,
+
+    mock_exa = MockExaEnvironment(
+        metadata=mock_meta,
         connections=params.bfs_connections)
 
-    result = executor.run([Group(params.work_with_span_input_data)], exa)
-    result_output = Output(result[0].rows)
+    mock_base_model_factory: Union[ModelFactoryProtocol, MagicMock] = create_autospec(ModelFactoryProtocol,
+                                                                                      _name="mock_base_model_factory")
+    mock_models: List[Union[transformers.AutoModel, MagicMock]] = [
+        create_autospec(transformers.AutoModel)
+        ]
+    mock_cast(mock_base_model_factory.from_pretrained).side_effect = mock_models
+
+    mock_tokenizer_factory: Union[ModelFactoryProtocol, MagicMock] = create_autospec(ModelFactoryProtocol)
+    mock_pipeline:  List[Union[transformers.AutoModel, MagicMock]] = [
+        create_autospec(Pipeline, side_effect=[params.tokenizer_model_output_df])
+        ]
+    mock_pipeline_factory: Union[Pipeline, MagicMock] = create_autospec(Pipeline,
+                                                                        side_effect=mock_pipeline)
+    udf = TokenClassificationUDF(exa=mock_exa,
+                                 batch_size=params.batch_size,
+                                 base_model=mock_base_model_factory,
+                                 tokenizer=mock_tokenizer_factory,
+                                 pipeline=mock_pipeline_factory,
+                                 work_with_spans=True)
+
+    udf.run(mock_ctx)
+    result = mock_ctx.output
+
+    assert result[0][-1] is None and len(result[0]) == len(mock_meta.output_columns)
+
     expected_output = Output(params.work_with_span_output_data)
-    n_input_columns = len(meta.input_columns) - 1
-    try:
-        assert (
-            OutputMatcher(result_output, n_input_columns) == expected_output,
-            params.mock_pipeline.counter == params.expected_model_counter)
-    finally:
-        params.mock_pipeline.counter = 0
+    actual_output = Output(result)
+    n_input_columns = len(mock_meta.input_columns) - 1
+    assert (
+            OutputMatcher(actual_output, n_input_columns) == expected_output,
+            mock_pipeline_factory.mock_calls == params.expected_model_counter)
+
 
 
 @pytest.mark.parametrize("params", [
     SingleModelSingleBatchIncomplete,
     SingleModelSingleBatchComplete,
-    SingleModelMultipleBatchIncomplete,
-    SingleModelMultipleBatchComplete,
-    MultipleModelSingleBatchIncomplete,
-    MultipleModelSingleBatchComplete,
-    MultipleModelMultipleBatchIncomplete,
-    MultipleModelMultipleBatchComplete,
-    MultipleModelMultipleBatchMultipleModelsPerBatch,
+    # SingleModelMultipleBatchIncomplete,
+    # SingleModelMultipleBatchComplete,
+    # MultipleModelSingleBatchIncomplete,
+    # MultipleModelSingleBatchComplete,
+    # MultipleModelMultipleBatchIncomplete,
+    # MultipleModelMultipleBatchComplete,
+    # MultipleModelMultipleBatchMultipleModelsPerBatch,
     SingleBucketFSConnMultipleSubdirSingleModelNameSingleBatch,
-    SingleBucketFSConnMultipleSubdirSingleModelNameMultipleBatch,
-    MultipleBucketFSConnSingleSubdirSingleModelNameSingleBatch,
-    MultipleBucketFSConnSingleSubdirSingleModelNameMultipleBatch,
-    MultipleStrategySingleModelNameSingleBatch,
-    MultipleStrategySingleModelNameMultipleBatch,
-    ErrorNotCachedSingleModelMultipleBatch,
-    ErrorNotCachedMultipleModelMultipleBatch,
-    ErrorOnPredictionMultipleModelMultipleBatch,
-    ErrorOnPredictionSingleModelMultipleBatch
+    # SingleBucketFSConnMultipleSubdirSingleModelNameMultipleBatch,
+    # MultipleBucketFSConnSingleSubdirSingleModelNameSingleBatch,
+    # MultipleBucketFSConnSingleSubdirSingleModelNameMultipleBatch,
+    # MultipleStrategySingleModelNameSingleBatch,
+    # MultipleStrategySingleModelNameMultipleBatch,
+    # ErrorNotCachedSingleModelMultipleBatch,
+    # ErrorNotCachedMultipleModelMultipleBatch,
+    # ErrorOnPredictionMultipleModelMultipleBatch,
+    # ErrorOnPredictionSingleModelMultipleBatch
 ])
 @patch('exasol.python_extension_common.connections.bucketfs_location.create_bucketfs_location_from_conn_object')
 @patch('exasol_transformers_extension.utils.bucketfs_operations.get_local_bucketfs_path')
 def test_token_classification(mock_local_path, mock_create_loc, params):
-
     mock_create_loc.side_effect = fake_bucketfs_location_from_conn_object
     mock_local_path.side_effect = fake_local_bucketfs_path
 
-    executor = UDFMockExecutor()
-    meta = create_mock_metadata(params.udf_wrapper)
+    mock_meta = create_mock_metadata()
+    input = params.input_data
+    mock_ctx = StandaloneMockContext(inp=input, metadata=mock_meta)
 
-    exa = MockExaEnvironment(
-        metadata=meta,
+
+    mock_exa = MockExaEnvironment(
+        metadata=mock_meta,
         connections=params.bfs_connections)
 
-    result = executor.run([Group(params.input_data)], exa)
-    result_output = Output(result[0].rows)
+    mock_base_model_factory: Union[ModelFactoryProtocol, MagicMock] = create_autospec(ModelFactoryProtocol,
+                                                                                      _name="mock_base_model_factory")
+    number_of_intendet_used_models = params.expected_model_counter# todo is this always same?
+    mock_models: List[Union[transformers.AutoModel, MagicMock]] = [
+        create_autospec(transformers.AutoModel) for i in range (0,number_of_intendet_used_models)
+        ]
+    print(mock_models)
+    mock_cast(mock_base_model_factory.from_pretrained).side_effect = mock_models
+
+    mock_tokenizer_factory: Union[ModelFactoryProtocol, MagicMock] = create_autospec(ModelFactoryProtocol)
+    print(params.batch_size)
+    print(params.work_with_span_input_data)
+    print(len(params.work_with_span_output_data))
+    print("tokenizer_model_output_df")
+    print(params.tokenizer_model_output_df)
+    mock_pipeline:  List[Union[transformers.AutoModel, MagicMock]] = [
+        create_autospec(Pipeline, side_effect=[params.tokenizer_model_output_df[i]]) for i in range (0,number_of_intendet_used_models)
+        ]
+    mock_pipeline_factory: Union[Pipeline, MagicMock] = create_autospec(Pipeline,
+                                                                        side_effect=mock_pipeline)
+    udf = TokenClassificationUDF(exa=mock_exa,
+                                 batch_size=params.batch_size,
+                                 base_model=mock_base_model_factory,
+                                 tokenizer=mock_tokenizer_factory,
+                                 pipeline=mock_pipeline_factory)
+
+    udf.run(mock_ctx)
+    result = mock_ctx.output
+    print(result)
+    assert result[0][-1] is None and len(result[0]) == len(mock_meta.output_columns)
+
     expected_output = Output(params.output_data)
-    n_input_columns = len(meta.input_columns) - 1
-    try:
-        assert (
-            OutputMatcher(result_output, n_input_columns) == expected_output,
-            params.mock_pipeline.counter == params.expected_model_counter)
-    finally:
-        params.mock_pipeline.counter = 0
+    actual_output = Output(result)
+    n_input_columns = len(mock_meta.input_columns) - 1
+    assert (
+            OutputMatcher(actual_output, n_input_columns) == expected_output,
+            mock_pipeline_factory.mock_calls == params.expected_model_counter)
