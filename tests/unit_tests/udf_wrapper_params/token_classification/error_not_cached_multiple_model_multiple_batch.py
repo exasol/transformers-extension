f8e72b0f6f86b265cc8fe4066148eba34ba989b6
MarleneKress79789 <marlene.kress@gmx.de>
2024-11-12 15:14:25 +0100

    converted error unit tests

--- tests/unit_tests/udf_wrapper_params/token_classification/error_not_cached_multiple_model_multiple_batch.py
+++ tests/unit_tests/udf_wrapper_params/token_classification/error_not_cached_multiple_model_multiple_batch.py
@@ -1,113 +1,54 @@
 from pathlib import PurePosixPath
 from exasol_udf_mock_python.connection import Connection
-from tests.unit_tests.udf_wrapper_params.token_classification.\
-    mock_token_classification import \
-    MockTokenClassificationFactory, MockTokenClassificationModel, MockPipeline
+from tests.unit_tests.udf_wrapper_params.token_classification.make_data_row_functions import make_input_row, \
+    make_output_row, make_input_row_with_span, make_output_row_with_span, bucketfs_conn, \
+    sub_dir, text_docid, text_start, text_end, agg_strategy_simple, make_model_output_for_one_input_row, make_number_of_strings, text_data
 
-
-def udf_wrapper():
-    from exasol_udf_mock_python.udf_context import UDFContext
-    from exasol_transformers_extension.udfs.models.token_classification_udf import \
-        TokenClassificationUDF
-    from tests.unit_tests.udf_wrapper_params.token_classification. \
-        mock_sequence_tokenizer import MockSequenceTokenizer
-    from tests.unit_tests.udf_wrapper_params.token_classification.\
-        error_not_cached_multiple_model_multiple_batch import \
-        ErrorNotCachedMultipleModelMultipleBatch as params
-
-    udf = TokenClassificationUDF(
-        exa,
-        batch_size=params.batch_size,
-        pipeline=params.mock_pipeline,
-        base_model=params.mock_factory,
-        tokenizer=MockSequenceTokenizer)
-
-    def run(ctx: UDFContext):
-        udf.run(ctx)
-
-def work_with_span_udf_wrapper():
-    from exasol_udf_mock_python.udf_context import UDFContext
-    from exasol_transformers_extension.udfs.models.token_classification_udf import \
-        TokenClassificationUDF
-    from tests.unit_tests.udf_wrapper_params.token_classification. \
-        mock_sequence_tokenizer import MockSequenceTokenizer
-    from tests.unit_tests.udf_wrapper_params.token_classification.\
-        error_not_cached_multiple_model_multiple_batch import \
-        ErrorNotCachedMultipleModelMultipleBatch as params
-
-    udf = TokenClassificationUDF(
-        exa,
-        work_with_spans=True,
-        batch_size=params.batch_size,
-        pipeline=params.mock_pipeline,
-        base_model=params.mock_factory,
-        tokenizer=MockSequenceTokenizer)
-
-    def run(ctx: UDFContext):
-        udf.run(ctx)
 
 class ErrorNotCachedMultipleModelMultipleBatch:
     """
     not cached error, multiple model, multiple batch
     """
-    expected_model_counter = 0
+    expected_model_counter = 1
     batch_size = 3
     data_size = 2
     n_entities = 3
-    agg_strategy = "simple"
 
-    token_docid = 1
-    start = 0
-    end = 20
+    bfs_conn1, bfs_conn2 = make_number_of_strings(bucketfs_conn, 2)
+    subdir1, subdir2 = make_number_of_strings(sub_dir, 2)
+    text1, text2 = make_number_of_strings(text_data, 2)
 
-    token_start = 2
-    token_end = 4
+    input_data = make_input_row(bucketfs_conn=bfs_conn1, sub_dir=subdir1, text_data=text1) * data_size + \
+                 make_input_row(bucketfs_conn=bfs_conn2, sub_dir=subdir2,
+                                model_name="non_existing_model",text_data=text2) * data_size
 
-    input_data = [(None, "bfs_conn1", "sub_dir1", "model1",
-                   "text1", agg_strategy)] * data_size + \
-                 [(None, "bfs_conn2", "sub_dir2", "non_existing_model",
-                   "text2", agg_strategy)] * data_size
-    output_data = [("bfs_conn1", "sub_dir1", "model1", "text1", agg_strategy,
-                    0, 6, "text1", "label1", 0.1, None)
-                   ] * n_entities * data_size + \
-                  [("bfs_conn2", "sub_dir2", "non_existing_model", "text2",
-                    agg_strategy, None, None, None, None, None, "Traceback")
-                   ] * n_entities * data_size
+    output_data = make_output_row(bucketfs_conn=bfs_conn1, sub_dir=subdir1, text_data=text1) * n_entities * data_size + \
+                  make_output_row(bucketfs_conn=bfs_conn2, sub_dir=subdir2,
+                                  model_name="non_existing_model",text_data=text2,
+                                  score=None, start=None, end=None, word=None, entity=None,
+                                  error_msg="Traceback") * n_entities * data_size
 
-    work_with_span_input_data = [(None, "bfs_conn1", "sub_dir1", "model1",
-                   "text", 1, 0, 6, agg_strategy)] * data_size + \
-                 [(None, "bfs_conn2", "sub_dir2", "non_existing_model",
-                   "text2", 1, 0, 6, agg_strategy)] * data_size
-    work_with_span_output_data = [("bfs_conn1", "sub_dir1", "model1", agg_strategy,
-                    "text", "label1", 0.1, token_docid, start+token_start, start+token_end, None)] * n_entities * data_size + \
-                  [("bfs_conn2", "sub_dir2", "non_existing_model", "text2",
-                    agg_strategy, None, None, None, None, None, "Traceback")
-                   ] * n_entities * data_size
+    work_with_span_input_data = make_input_row_with_span(bucketfs_conn=bfs_conn1, sub_dir=subdir1,text_data=text1) * data_size + \
+                                make_input_row_with_span(bucketfs_conn=bfs_conn2, sub_dir=subdir2,
+                                                         model_name="non_existing_model",text_data=text2) * data_size
+
+    work_with_span_output_data1 = make_output_row_with_span(bucketfs_conn=bfs_conn1, sub_dir=subdir1) * n_entities * data_size
+    work_with_span_output_data2 = [(bfs_conn2, subdir2, "non_existing_model", text_docid, text_start, text_end, agg_strategy_simple,
+                                   None, None, None, text_docid, None, None,"Traceback")] * n_entities * data_size
+    work_with_span_output_data = work_with_span_output_data2 + work_with_span_output_data2
+
+    tokenizer_model_output_df_model1 =  [make_model_output_for_one_input_row(number_entities=n_entities) * data_size]
+    tokenizer_model_output_df_model2_batch1 = [] # no model loaded so no model to output anything
+    tokenizer_model_output_df_model2_batch2 =  [] # no model loaded so no model to output anything
+
+    tokenizer_models_output_df = [tokenizer_model_output_df_model1, tokenizer_model_output_df_model2_batch1, tokenizer_model_output_df_model2_batch2]
+
 
     tmpdir_name = "_".join(("/tmpdir", __qualname__))
-    base_cache_dir1 = PurePosixPath(tmpdir_name, "bfs_conn1")
-    base_cache_dir2 = PurePosixPath(tmpdir_name, "bfs_conn2")
+    base_cache_dir1 = PurePosixPath(tmpdir_name, bfs_conn1)
+    base_cache_dir2 = PurePosixPath(tmpdir_name, bfs_conn2)
     bfs_connections = {
-        "bfs_conn1": Connection(address=f"file://{base_cache_dir1}"),
-        "bfs_conn2": Connection(address=f"file://{base_cache_dir2}")
+        bfs_conn1: Connection(address=f"file://{base_cache_dir1}"),
+        bfs_conn2: Connection(address=f"file://{base_cache_dir2}")
     }
-    mock_factory = MockTokenClassificationFactory({
-        PurePosixPath(base_cache_dir1, "sub_dir1", "model1_token-classification"):
-            MockTokenClassificationModel(
-                starts=[0] * n_entities,
-                ends=[6] * n_entities,
-                words=["text1"] * n_entities,
-                entities=["label1"] * n_entities,
-                scores=[0.1] * n_entities),
-        PurePosixPath(base_cache_dir2, "sub_dir2", "model2_token-classification"):
-            MockTokenClassificationModel(
-                starts=[0] * n_entities,
-                ends=[6] * n_entities,
-                words=["text2"] * n_entities,
-                entities=["label2"] * n_entities,
-                scores=[0.2] * n_entities),
-    })
 
-    mock_pipeline = MockPipeline
-    udf_wrapper = udf_wrapper
-    work_with_span_udf_wrapper = work_with_span_udf_wrapper
--- tests/unit_tests/udf_wrapper_params/token_classification/error_not_cached_single_model_multiple_batch.py
+++ tests/unit_tests/udf_wrapper_params/token_classification/error_not_cached_single_model_multiple_batch.py
@@ -1,78 +1,31 @@
 from pathlib import PurePosixPath
 from exasol_udf_mock_python.connection import Connection
-from tests.unit_tests.udf_wrapper_params.token_classification.\
-    mock_token_classification import \
-    MockTokenClassificationFactory, MockTokenClassificationModel, MockPipeline
+from tests.unit_tests.udf_wrapper_params.token_classification.make_data_row_functions import make_input_row, \
+    make_output_row, make_input_row_with_span, make_output_row_with_span, bucketfs_conn, \
+    text_docid, text_start, text_end, agg_strategy_simple, sub_dir, model_name, make_model_output_for_one_input_row, make_number_of_strings
 
-
-def udf_wrapper():
-    from exasol_udf_mock_python.udf_context import UDFContext
-    from exasol_transformers_extension.udfs.models.token_classification_udf import \
-        TokenClassificationUDF
-    from tests.unit_tests.udf_wrapper_params.token_classification. \
-        mock_sequence_tokenizer import MockSequenceTokenizer
-    from tests.unit_tests.udf_wrapper_params.token_classification.\
-        error_not_cached_single_model_multiple_batch import \
-        ErrorNotCachedSingleModelMultipleBatch as params
-
-    udf = TokenClassificationUDF(
-        exa,
-        batch_size=params.batch_size,
-        pipeline=params.mock_pipeline,
-        base_model=params.mock_factory,
-        tokenizer=MockSequenceTokenizer)
-
-    def run(ctx: UDFContext):
-        udf.run(ctx)
-
-def work_with_span_udf_wrapper():
-    from exasol_udf_mock_python.udf_context import UDFContext
-    from exasol_transformers_extension.udfs.models.token_classification_udf import \
-        TokenClassificationUDF
-    from tests.unit_tests.udf_wrapper_params.token_classification. \
-        mock_sequence_tokenizer import MockSequenceTokenizer
-    from tests.unit_tests.udf_wrapper_params.token_classification.\
-        error_not_cached_single_model_multiple_batch import \
-        ErrorNotCachedSingleModelMultipleBatch as params
-
-    udf = TokenClassificationUDF(
-        exa,
-        work_with_spans=True,
-        batch_size=params.batch_size,
-        pipeline=params.mock_pipeline,
-        base_model=params.mock_factory,
-        tokenizer=MockSequenceTokenizer)
-
-    def run(ctx: UDFContext):
-        udf.run(ctx)
 
 class ErrorNotCachedSingleModelMultipleBatch:
     """
     not cached error, single model, multiple batch
     """
-    expected_model_counter = 1
+    expected_model_counter = 0
     batch_size = 2
     data_size = 5
     n_entities = 3
-    agg_strategy = "simple"
 
-    token_docid = 1
-    start = 0
-    end = 20
+    input_data = make_input_row(model_name="non_existing_model") * data_size
+    output_data = make_output_row(model_name="non_existing_model",
+                                  score=None, start=None, end=None, word=None, entity=None,
+                                  error_msg="Traceback") * n_entities * data_size
 
-    token_start = 2
-    token_end = 4
+    work_with_span_input_data = make_input_row_with_span(model_name="non_existing_model") * data_size
+    work_with_span_output_data =  [(bucketfs_conn, sub_dir, "non_existing_model",
+                                text_docid, text_start, text_end, agg_strategy_simple,
+                                None, None, None, text_docid, None, None,
+                                "Traceback")] * n_entities * data_size
 
-    input_data = [(None, "bfs_conn1", "sub_dir1", "non_existing_model",
-                   "text", agg_strategy)] * data_size
-    output_data = [("bfs_conn1", "sub_dir1", "non_existing_model", "text",
-                    agg_strategy, None, None, None, None, None, "Traceback")
-                   ] * n_entities * data_size
-
-    work_with_span_input_data = [(None, "bfs_conn1", "sub_dir1", "non_existing_model",
-                   "text", 1, 0, 6, agg_strategy)] * data_size
-    work_with_span_output_data = [("bfs_conn1", "sub_dir1", "model1", agg_strategy,
-                    "text", None, None, None, None, None, "Traceback")] * n_entities * data_size
+    tokenizer_models_output_df = [] # no model loaded so no model to output anything
 
     tmpdir_name = "_".join(("/tmpdir", __qualname__))
     base_cache_dir1 = PurePosixPath(tmpdir_name, "bfs_conn1")
@@ -80,16 +33,4 @@ class ErrorNotCachedSingleModelMultipleBatch:
         "bfs_conn1": Connection(address=f"file://{base_cache_dir1}")
     }
 
-    mock_factory = MockTokenClassificationFactory({
-        PurePosixPath(base_cache_dir1, "sub_dir1", "model1_token-classification"):
-            MockTokenClassificationModel(
-                starts=[0] * n_entities,
-                ends=[6] * n_entities,
-                words=["text"] * n_entities,
-                entities=["label1"] * n_entities,
-                scores=[0.1] * n_entities),
-    })
 
-    mock_pipeline = MockPipeline
-    udf_wrapper = udf_wrapper
-    work_with_span_udf_wrapper = work_with_span_udf_wrapper
