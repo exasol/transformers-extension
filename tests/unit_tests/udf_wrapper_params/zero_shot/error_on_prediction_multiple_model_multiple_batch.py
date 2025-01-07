d220b6641909ae0ded5e198d9372b70c66acf6fb
MarleneKress79789 <marlene.kress@gmx.de>
2025-01-07 14:42:17 +0100

    changed zero shot unit tests

--- tests/unit_tests/udf_wrapper_params/zero_shot/error_on_prediction_multiple_model_multiple_batch.py
+++ tests/unit_tests/udf_wrapper_params/zero_shot/error_on_prediction_multiple_model_multiple_batch.py
@@ -1,29 +1,9 @@
 from pathlib import PurePosixPath
 from exasol_udf_mock_python.connection import Connection
-from tests.unit_tests.udf_wrapper_params.zero_shot.mock_zero_shot import \
-    MockZeroShotFactory, MockZeroShotModel, MockPipeline
 
-
-def udf_wrapper():
-    from exasol_udf_mock_python.udf_context import UDFContext
-    from exasol_transformers_extension.udfs.models.\
-        zero_shot_text_classification_udf import ZeroShotTextClassificationUDF
-    from tests.unit_tests.udf_wrapper_params.zero_shot.mock_sequence_tokenizer \
-        import MockSequenceTokenizer
-    from tests.unit_tests.udf_wrapper_params.zero_shot.\
-        error_on_prediction_multiple_model_multiple_batch import \
-        ErrorOnPredictionMultipleModelMultipleBatch as params
-
-    udf = ZeroShotTextClassificationUDF(
-        exa,
-        batch_size=params.batch_size,
-        pipeline=params.mock_pipeline,
-        base_model=params.mock_factory,
-        tokenizer=MockSequenceTokenizer)
-
-    def run(ctx: UDFContext):
-        udf.run(ctx)
-
+from tests.unit_tests.udf_wrapper_params.zero_shot.make_data_row_fuctions import make_number_of_strings, sub_dir, \
+    model_name, text_data, label, make_input_row, score, make_output_row, make_model_output_for_one_input_row, \
+    bucketfs_conn
 
 class ErrorOnPredictionMultipleModelMultipleBatch:
     """
@@ -33,29 +13,33 @@ class ErrorOnPredictionMultipleModelMultipleBatch:
     batch_size = 3
     data_size = 2
 
-    input_data = [(None, "bfs_conn1", "sub_dir1", "model1",
-                   "text1", "label1")] * data_size + \
-                 [(None, "bfs_conn2", "sub_dir2", "model2",
-                   "error on pred", "label2")] * data_size
-    output_data = [("bfs_conn1", "sub_dir1", "model1", "text1",
-                    "label1", "label1", 0.1, 1, None)] * data_size + \
-                  [("bfs_conn2", "sub_dir2", "model2", "error on pred",
-                    "label2", None, None, None, "Traceback")] * data_size
+    sub_dir1, sub_dir2 = make_number_of_strings(sub_dir, 2)
+    model_name1, model_name2 = make_number_of_strings(model_name, 2)
+    label1, label2 = make_number_of_strings(label, 2)
+
+    input_data = make_input_row(sub_dir=sub_dir1, model_name=model_name1,
+                                text_data=text_data, candidate_labels=label1) * data_size + \
+                 make_input_row(sub_dir=sub_dir2, model_name=model_name2,
+                                text_data="error on pred", candidate_labels=label2) * data_size
+
+    output_data = make_output_row(sub_dir=sub_dir1, model_name=model_name1,
+                                  text_data=text_data, candidate_labels=label1,
+                                  label=label1, score=score) * data_size + \
+                  make_output_row(sub_dir=sub_dir2, model_name=model_name2,
+                                  text_data="error on pred", candidate_labels=label2,
+                                  label=None,  score=None, rank=None, error_msg="Traceback") * data_size
+
+    zero_shot_model_output_df_batch1 = [make_model_output_for_one_input_row(candidate_labels=label1, score=score) * data_size,
+                                       []]
+    zero_shot_model_output_df_batch2 = []
+
+    zero_shot_models_output_df = [zero_shot_model_output_df_batch1, zero_shot_model_output_df_batch2]
 
     tmpdir_name = "_".join(("/tmpdir", __qualname__))
-    base_cache_dir1 = PurePosixPath(tmpdir_name, "bfs_conn1")
-    base_cache_dir2 = PurePosixPath(tmpdir_name, "bfs_conn2")
+    base_cache_dir1 = PurePosixPath(tmpdir_name, bucketfs_conn)
     bfs_connections = {
-        "bfs_conn1": Connection(address=f"file://{base_cache_dir1}"),
-        "bfs_conn2": Connection(address=f"file://{base_cache_dir2}")
+        bucketfs_conn: Connection(address=f"file://{base_cache_dir1}"),
     }
-    mock_factory = MockZeroShotFactory({
-        PurePosixPath(base_cache_dir1, "sub_dir1", "model1_zero-shot-classification"):
-            MockZeroShotModel([{"labels": "label1", "scores": 0.1}]),
-        PurePosixPath(base_cache_dir2, "sub_dir2", "model2_zero-shot-classification"):
-            MockZeroShotModel([{"labels": "label2", "scores": 0.2}])
-    })
 
-    mock_pipeline = MockPipeline
-    udf_wrapper = udf_wrapper
+
 
