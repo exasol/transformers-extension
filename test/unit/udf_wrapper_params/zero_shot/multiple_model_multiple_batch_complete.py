c082ccec3ce9f68c170e144060ed4fa2fd3d9407
MarleneKress79789 <marlene.kress@gmx.de>
2025-03-17 10:58:20 +0100

    zero shot unit tests

--- test/unit/udf_wrapper_params/zero_shot/multiple_model_multiple_batch_complete.py
+++ test/unit/udf_wrapper_params/zero_shot/multiple_model_multiple_batch_complete.py
@@ -1,11 +1,13 @@
+import dataclasses
 from pathlib import PurePosixPath
-from exasol_udf_mock_python.connection import Connection
 
 from test.unit.udf_wrapper_params.zero_shot.make_data_row_functions import make_number_of_strings, sub_dir, \
     model_name, text_data, label, make_input_row, score, make_output_row, make_model_output_for_one_input_row, \
     bucketfs_conn, make_input_row_with_span, make_output_row_with_span
 
+from exasol_udf_mock_python.connection import Connection
 
+@dataclasses.dataclass
 class MultipleModelMultipleBatchComplete:
     """
     multiple model, multiple batch, last batch complete
@@ -41,8 +43,10 @@ class MultipleModelMultipleBatchComplete:
                                  make_output_row_with_span(sub_dir=sub_dir2, model_name=model_name2,
                                                            label=label2,  score=score + 0.1) * data_size
 
-    zero_shot_model_output_df_model1 = [make_model_output_for_one_input_row(candidate_labels=label1, score=score) * data_size]
-    zero_shot_model_output_df_model2 = [make_model_output_for_one_input_row(candidate_labels=label2, score=score+0.1) * data_size]
+    zero_shot_model_output_df_model1 = [make_model_output_for_one_input_row(candidate_labels=label1,
+                                                                            score=score) * data_size]
+    zero_shot_model_output_df_model2 = [make_model_output_for_one_input_row(candidate_labels=label2,
+                                                                            score=score+0.1) * data_size]
 
     zero_shot_models_output_df = [zero_shot_model_output_df_model1, zero_shot_model_output_df_model2]
 
