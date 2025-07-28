c83b9206694c2cddeed84a6f50cf0b77e6fef302
MarleneKress79789 <marlene.kress@gmx.de>
2025-07-28 14:18:10 +0200

    add files and stubs

---
+++ exasol_transformers_extension/udfs/models/ls_models_udf.py
@@ -0,0 +1,93 @@
+from typing import Tuple
+
+import exasol.python_extension_common.connections.bucketfs_location as bfs_loc
+import transformers
+
+from exasol_transformers_extension.utils.bucketfs_model_specification import (
+    BucketFSModelSpecificationFactory,
+)
+from exasol_transformers_extension.utils.huggingface_hub_bucketfs_model_transfer_sp import (
+    HuggingFaceHubBucketFSModelTransferSPFactory,
+)
+from exasol_transformers_extension.utils.model_factory_protocol import (
+    ModelFactoryProtocol,
+)
+
+
+class ListModelsUDF:
+    """
+    UDF which list all transformers models installed with the Transformers Extension in the BucketFS.
+    Must be called with the following Input Parameter:
+
+    | sub_dir                 | bfs_conn            |
+    -------------------------------------------------
+    | directory to save model | BucketFS connection |
+
+    returns a table of  <sub_dir/model_name> , <path of model BucketFS>
+    path, subdir, version, task_name, model_name ?
+
+    udf bekommt bucketfs-connection
+
+    function bekommt bucketfs-model-path?
+    """
+
+    def __init__(
+        self,
+        exa,
+        #tokenizer_factory: ModelFactoryProtocol = transformers.AutoTokenizer,
+        #huggingface_hub_bucketfs_model_transfer: HuggingFaceHubBucketFSModelTransferSPFactory = HuggingFaceHubBucketFSModelTransferSPFactory(),
+        current_model_specification_factory: BucketFSModelSpecificationFactory = BucketFSModelSpecificationFactory(),
+    ):
+        self._exa = exa
+        #self._tokenizer_factory = tokenizer_factory
+        #self._huggingface_hub_bucketfs_model_transfer = (
+        #    huggingface_hub_bucketfs_model_transfer
+        #)
+        self._current_model_specification_factory = current_model_specification_factory
+
+    def run(self, ctx) -> None:
+        while True:
+            model_path = self._download_model(ctx)
+            ctx.emit(*model_path)
+            if not ctx.next():
+                break
+
+    def _download_model(self, ctx) -> Tuple[str, str]:
+        # parameters
+        bfs_conn = ctx.bfs_conn  # BucketFS connection
+        token_conn = ctx.token_conn  # name of token connection
+        current_model_specification = self._current_model_specification_factory.create(
+            ctx.model_name, ctx.task_type, bfs_conn, ctx.sub_dir
+        )  # specifies details of Huggingface model
+
+        model_factory = current_model_specification.get_model_factory()
+        # extract token from the connection if token connection name is given.
+        # note that, token is required for private models. It doesn't matter
+        # whether there is a token for public model or even what the token is.
+        token = False
+        if token_conn:
+            token_conn_obj = self._exa.get_connection(token_conn)
+            token = token_conn_obj.password
+
+        # set model path in buckets
+        model_path = current_model_specification.get_bucketfs_model_save_path()
+
+        # create bucketfs location
+        bfs_conn_obj = self._exa.get_connection(bfs_conn)
+        bucketfs_location = bfs_loc.create_bucketfs_location_from_conn_object(
+            bfs_conn_obj
+        )
+
+        # download base model and tokenizer into the model path
+        with self._huggingface_hub_bucketfs_model_transfer.create(
+            bucketfs_location=bucketfs_location,
+            model_specification=current_model_specification,
+            model_path=model_path,
+            token=token,
+        ) as downloader:
+            for model in [model_factory, self._tokenizer_factory]:
+                downloader.download_from_huggingface_hub(model)
+            # upload model files to BucketFS
+            model_tar_file_path = downloader.upload_to_bucketfs()
+
+        return str(model_path), str(model_tar_file_path)
