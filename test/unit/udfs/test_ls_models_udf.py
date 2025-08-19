de6b02fe77689cb3ad74de9f0b419ce34cfc8f7e
MarleneKress79789 <marlene.kress@gmx.de>
2025-08-19 15:25:18 +0200

    work on unit test

--- test/unit/udfs/test_ls_models_udf.py
+++ test/unit/udfs/test_ls_models_udf.py
@@ -1,3 +1,5 @@
+import os
+
 from test.unit.utils.utils_for_base_udf_tests import (
     create_mock_metadata,
 )
@@ -79,19 +81,26 @@ def test_ls_udf(tmpdir_factory):
     token_model_specs = model_params.token_model_specs
     qa_model_specs = model_params.q_a_model_specs #todo these could be mocks
     sub_dir = "subdir"
-
     mock_bucketfs_location = tmpdir_factory.mktemp("test_list_models")
     # real bucketfs would create these dirs, but tempdir does not
     mock_bucketfs_location.mkdir(Path(sub_dir))
     mock_bucketfs_location.mkdir(Path(sub_dir) / "dslim")
     mock_bucketfs_location.mkdir(Path(sub_dir) / "deepset")
+    os.mknod(mock_bucketfs_location / sub_dir / token_model_specs.get_model_specific_path_suffix().with_suffix(".tar.gz"))
+    os.mknod(mock_bucketfs_location / sub_dir / qa_model_specs.get_model_specific_path_suffix().with_suffix(".tar.gz"))
+    for item in os.walk(mock_bucketfs_location):
+        print(item)
+    from test.utils.mock_connections import create_mounted_bucketfs_connection
+    import pathlib
 
-    bucketfs_connection = Connection(address=str(mock_bucketfs_location))#f"file:///test_ls")
+    #url_of_mock_bucketfs = pathlib.Path(os.path.abspath(mock_bucketfs_location)).as_uri()
+    bucketfs_conn = create_mounted_bucketfs_connection(base_path=mock_bucketfs_location)
+    #bucketfs_connection = Connection(address=str(mock_bucketfs_location),user="{}", password="{}")#f"file:///test_ls")
     bfs_conn_name = "bfs_conn"
 
     mock_meta = create_mock_metadata()
     mock_exa = create_mock_exa_environment(
-        mock_meta, {bfs_conn_name: bucketfs_connection}
+        mock_meta, {bfs_conn_name: bucketfs_conn}
     )
     input_data = [(bfs_conn_name, sub_dir)]
     mock_ctx = create_mock_udf_context(input_data, mock_meta)
