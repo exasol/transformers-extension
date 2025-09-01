8298618ca7ae2e5b3a2834ba9939926ca4331868
MarleneKress79789 <marlene.kress@gmx.de>
2025-09-01 15:29:48 +0200

    change task_type search

--- exasol_transformers_extension/utils/model_specification.py
+++ exasol_transformers_extension/utils/model_specification.py
@@ -1,8 +1,22 @@
 """Class ModelSpecification describing a specific model."""
 
 from pathlib import PurePosixPath, Path
+from dataclasses import dataclass
 
 import transformers
+
+
+@dataclass
+class ModelTypeData:
+        model_factory_dict = {
+            "fill-mask": transformers.AutoModelForMaskedLM,
+            "translation": transformers.AutoModelForSeq2SeqLM,
+            "zero-shot-classification": transformers.AutoModelForSequenceClassification,
+            "text-classification": transformers.AutoModelForSequenceClassification,
+            "question-answering": transformers.AutoModelForQuestionAnswering,
+            "text-generation": transformers.AutoModelForCausalLM,
+            "token-classification": transformers.AutoModelForTokenClassification
+        }
 
 
 class ModelSpecification:
@@ -62,23 +76,13 @@ class ModelSpecification:
         sets model factory depending on the task_type of the specific model
         """
         model_task_type = self.task_type
-        if model_task_type == "fill-mask":
-            model_factory = transformers.AutoModelForMaskedLM
-        elif model_task_type == "translation":
-            model_factory = transformers.AutoModelForSeq2SeqLM
-        elif model_task_type == "zero-shot-classification":
-            model_factory = transformers.AutoModelForSequenceClassification
-        elif model_task_type == "text-classification":
-            model_factory = transformers.AutoModelForSequenceClassification
-        elif model_task_type == "question-answering":
-            model_factory = transformers.AutoModelForQuestionAnswering
-        elif model_task_type == "text-generation":
-            model_factory = transformers.AutoModelForCausalLM
-        elif model_task_type == "token-classification":
-            model_factory = transformers.AutoModelForTokenClassification
-        else:
+        try:
+            model_factory = ModelTypeData.model_factory_dict[model_task_type]
+        except KeyError:
             model_factory = transformers.AutoModel
+
         return model_factory
+
 
 def create_model_specs_from_path(model_path: Path, sub_dir) -> ModelSpecification:
         path_parts = model_path.parts
@@ -94,10 +98,42 @@ def create_model_specs_from_path(model_path: Path, sub_dir) -> ModelSpecificatio
 
         name_prefix = "/".join(path_parts[subdir_index+1:-1])
         model_specific_path_suffix = path_parts[-1].split('.')[0]
-        # we know the model_specific_path_suffix includes at least on "_" followed by the task_name
-        # todo check for "_known_task_type" first, if not found split at last "_"
-        model_specific_path_suffix_split = model_specific_path_suffix.split("_")
-        model_name = "/".join([name_prefix, "".join(model_specific_path_suffix_split[0:-1])])
 
-        task_name = model_specific_path_suffix_split[-1]
+        # find known task_names in the model_specific_path_suffix:
+        found_task_names = [key for key, _ in ModelTypeData.model_factory_dict if key in model_specific_path_suffix]
+
+        # if no known_task_type was found, our best guess is tp split the model_specific_path_suffix on "_"
+        # and select task_type and model_name accordingly, because
+        # we know the model_specific_path_suffix includes at least on "_" followed by the task_name
+        def best_guess_model_specs(model_specific_path_suffix):
+            try:
+                model_specific_path_suffix_split = model_specific_path_suffix.split("_")
+                model_name = "/".join([name_prefix, "".join(model_specific_path_suffix_split[0:-1])])
+                task_name = model_specific_path_suffix_split[-1]
+                return model_name, task_name
+            except:
+                # todo return error here
+                print("couldn't find task name in path")
+        if not found_task_names:
+            try:
+                model_name, task_name = best_guess_model_specs(model_specific_path_suffix)
+            except:
+                # todo rethrow?
+                print("couldn't find task name in path")
+
+        # if we found known_task_type in the path, check if one is on the end of the model_specific_path_suffix,
+        # and declare this one as the task_type.
+        # disregard found_task_names form other positions in the model_specific_path_suffix
+        for found_task_name in found_task_names:
+            if model_specific_path_suffix.endswith("_" + found_task_name):
+                model_name = "/".join([name_prefix, model_specific_path_suffix.removesuffix("_" + task_name)])
+                task_name = found_task_name
+                break
+        if not task_name or not model_name:
+            try:
+                model_name, task_name = best_guess_model_specs(model_specific_path_suffix)
+            except:
+                # todo rethrow?
+                print("couldn't find task name in path")
+
         return ModelSpecification(model_name, task_name)
