818f1610edc3648edf67dd96d6988722cb919c51
MarleneKress79789 <marlene.kress@gmx.de>
2025-09-09 13:02:24 +0200

    refactor ls_models_udf

--- exasol_transformers_extension/udfs/models/ls_models_udf.py
+++ exasol_transformers_extension/udfs/models/ls_models_udf.py
@@ -3,6 +3,7 @@ import traceback
 from pathlib import Path
 
 import exasol.python_extension_common.connections.bucketfs_location as bfs_loc
+from exasol.bucketfs._path import PathLike
 
 from exasol_transformers_extension.utils import bucketfs_operations
 from exasol_transformers_extension.utils.model_specification import create_model_specs_from_path
@@ -18,7 +19,7 @@ class ListModelsUDF:
     | directory where models are | BucketFS connection |
 
     returns a table of:
-    bucketfs_conn, sub_dir, model_name, task_name, path of model BucketFS
+    bucketfs_conn, sub_dir, model_name, task_name, path of model in BucketFS
     """
 
     def __init__(
@@ -26,32 +27,15 @@ class ListModelsUDF:
         exa,
     ):
         self._exa = exa
+        self._output = []
         self._error_message = None
 
     def run(self, ctx) -> None:
-        model_path_list = self._list_models(ctx)
-        for model_info in model_path_list:
+        self._list_models(ctx)
+        for model_info in self._output:
             ctx.emit(*model_info)
 
-    def _list_models(self, ctx):
-        # parameters
-        output = []
-        bfs_conn_name = ctx.bucketfs_conn  # BucketFS connection
-        sub_dir = str(ctx.sub_dir)
-
-        # create bucketfs location
-        bfs_conn_obj = self._exa.get_connection(bfs_conn_name)
-        print("bfs_conn_obj:")
-        print(bfs_conn_obj)
-        bucketfs_location = bfs_loc.create_bucketfs_location_from_conn_object(
-            bfs_conn_obj
-        )
-
-        if not sub_dir:
-            self._error_message = "sub_dir cant be an empty string" #-> disallow "" this in creation of path at some point?
-            output.append([bfs_conn_name, sub_dir, "", "", "", self._error_message])
-            return output
-
+    def _search_modelpaths_in_dir(self, sub_dir: str, bucketfs_location: PathLike):
         model_paths_list = []
         for main_dir, sub_dirs, files in os.walk(Path((bucketfs_location.as_udf_path() + "/" + sub_dir))):
             if files: #this means there is at least 1 file here
@@ -60,23 +44,45 @@ class ListModelsUDF:
                     # but always have a config.json
                     # https://huggingface.co/docs/diffusers/main/using-diffusers/other-formats
                     if file.endswith("config.json"):
-
-                    # some models have multiple config files
-                    # todo main_dir might be different
+                        # some models have multiple config files
+                        # todo main_dir might be different for different config_files. take highest level one?
                         if not main_dir in model_paths_list:
                             model_paths_list.append(main_dir)
-        if not model_paths_list:
-            self._error_message = "no models in this subdir" #todo do we want this message? or just return
-            output.append([bfs_conn_name, sub_dir, "", "", "", self._error_message])
-            return output
+        return model_paths_list
 
-        print(model_paths_list)
+
+    def _parse_model_info_from_path(self, model_paths_list: list[str], sub_dir: str, bfs_conn_name: str):
         for model_path in model_paths_list:
             try:
                 model_spec = create_model_specs_from_path(Path(model_path), sub_dir)
-                output.append([bfs_conn_name, sub_dir, model_spec.model_name, model_spec.task_type, model_path,
+                self._output.append([bfs_conn_name, sub_dir, model_spec.model_name, model_spec.task_type, model_path,
                                self._error_message])
             except Exception as exc:
                 self._error_message = traceback.format_exc()
-                output.append([bfs_conn_name, sub_dir, "", "", model_path, self._error_message])
-        return output
+                self._output.append([bfs_conn_name, sub_dir, "", "", model_path, self._error_message])
+
+
+    def _list_models(self, ctx):
+        # parameters
+        bfs_conn_name = ctx.bucketfs_conn  # BucketFS connection
+        sub_dir = str(ctx.sub_dir)
+
+        # create bucketfs location
+        bfs_conn_obj = self._exa.get_connection(bfs_conn_name)
+        bucketfs_location = bfs_loc.create_bucketfs_location_from_conn_object(
+            bfs_conn_obj
+        )
+
+        if not sub_dir:
+            self._error_message = "sub_dir cant be an empty string" #-> disallow "" this in creation of path at some point?
+            self._output.append([bfs_conn_name, sub_dir, "", "", "", self._error_message])
+            return self._output
+
+        model_paths_list = self._search_modelpaths_in_dir(sub_dir, bucketfs_location)
+
+        if not model_paths_list:
+            self._error_message = "no models in this subdir" #todo do we want this message? or just return empty?
+            self._output.append([bfs_conn_name, sub_dir, "", "", "", self._error_message])
+            return self._output
+
+        self._parse_model_info_from_path(model_paths_list, sub_dir, bfs_conn_name)
