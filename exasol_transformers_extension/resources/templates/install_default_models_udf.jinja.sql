f783139d1b1a0703f8b4b05287ddca0a723efee0
MarleneKress79789 <marlene.kress@gmx.de>
2026-02-12 12:51:47 +0100

    udf input

--- exasol_transformers_extension/resources/templates/install_default_models_udf.jinja.sql
+++ exasol_transformers_extension/resources/templates/install_default_models_udf.jinja.sql
@@ -1,5 +1,5 @@
-CREATE OR REPLACE {{ language_alias }} SET SCRIPT "TE_INSTALL_DEFAULT_MODEL_UDF"(
-) EMITS (
+CREATE OR REPLACE {{ language_alias }} SCALAR SCRIPT "TE_INSTALL_DEFAULT_MODEL_UDF"(...)
+       EMITS (
     model_path_in_udfs VARCHAR(2000000),
     model_path_of_tar_file_in_bucketfs VARCHAR(2000000)
 ) AS
