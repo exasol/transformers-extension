c2166f6fda17a9956adc783c5491589864fb4462
MarleneKress79789 <marlene.kress@gmx.de>
2026-06-12 10:15:46 +0200

    improve path handling

--- exasol_transformers_extension/deployment/write_create_script.py
+++ exasol_transformers_extension/deployment/write_create_script.py
@@ -9,20 +9,23 @@ from exasol_transformers_extension.deployment.script_deployment_queries import (
 )
 
 
-def write_create_script():
+def write_create_script(root_dir=None) -> Path:
     """
-    Writes SQL queries for the creation of all udf script to create_script.sql
+    Writes SQL queries for the creation of all udf script to
+    <root_dir>/"deployment/create_script.sql"create_script.sql
     """
     print("Write create script")
     sdq_creator = ScriptDeploymentQueries(
         language_alias="PYTHON3_TE", use_spans=True, install_all_scripts=True
     )  # todo wich udfs do we want to deploy with this script?
 
-    root_dir = Path(__file__).resolve().parent.parent
+    if not root_dir:
+        root_dir = Path(__file__).resolve().parent.parent
     script_path = root_dir / "deployment/create_script.sql"
     sdq_creator.write_create_sql_script(script_path)
     print("create_script written.")
+    return script_path
 
 
 if __name__ == "__main__":
-    write_create_script()
+    write_create_script(None)
