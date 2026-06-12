d043811369e8d946bf4b1c7f65bd82e5505e44ce
MarleneKress79789 <marlene.kress@gmx.de>
2026-06-12 12:25:59 +0200

    improve path handling

--- exasol_transformers_extension/deployment/write_create_script.py
+++ exasol_transformers_extension/deployment/write_create_script.py
@@ -1,7 +1,7 @@
 """
 Writes SQL queries for the creation of all udf script to create_script.sql
 """
-
+import os
 from pathlib import Path
 
 from exasol_transformers_extension.deployment.script_deployment_queries import (
@@ -21,7 +21,12 @@ def write_create_script(root_dir=None) -> Path:
 
     if not root_dir:
         root_dir = Path(__file__).resolve().parent.parent
-    script_path = root_dir / "deployment/create_script.sql"
+
+    dir_path = root_dir / "deployment"
+
+    if not os.path.exists(dir_path):  # create folders if not exists
+        os.makedirs(dir_path)
+    script_path = dir_path / "create_script.sql"
     sdq_creator.write_create_sql_script(script_path)
     print("create_script written.")
     return script_path
