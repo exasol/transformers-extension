c760d88ec8a36217a681aa8576bc31edac81fc43
MarleneKress79789 <marlene.kress@gmx.de>
2026-08-19 15:29:28 +0200

    fix templates

--- exasol_transformers_extension/resources/templates/ai_answer_udf.jinja.sql
+++ exasol_transformers_extension/resources/templates/ai_answer_udf.jinja.sql
@@ -1,7 +1,6 @@
 CREATE OR REPLACE {{ language_alias }} SET SCRIPT "AI_ANSWER"(
     question VARCHAR(2000000),
     context_text VARCHAR(2000000)
-    ORDER BY {{ ordered_columns | join(" ASC,") }} ASC
 )EMITS (
     question VARCHAR(2000000),
     context_text VARCHAR(2000000),
--- exasol_transformers_extension/resources/templates/ai_translate_udf.jinja.sql
+++ exasol_transformers_extension/resources/templates/ai_translate_udf.jinja.sql
@@ -1,8 +1,7 @@
 CREATE OR REPLACE {{ language_alias }} SET SCRIPT "AI_TRANSLATE"(
     text_data VARCHAR(2000000),
     source_language VARCHAR(2000000),
-    target_language VARCHAR(2000000),
-    ORDER BY {{ ordered_columns | join(" ASC,") }} ASC
+    target_language VARCHAR(2000000)
 )EMITS (
     text_data VARCHAR(2000000),
     source_language VARCHAR(2000000),
