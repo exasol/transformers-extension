4d2ba4150f240bc6bcd9f6e89818724cc2f1a53c
Torsten Kilias <torsten.kilias@exasol.com>
2026-08-27 11:58:26 +0200

    Remove install default model test from saas ci

--- noxconfig.py
+++ noxconfig.py
@@ -55,7 +55,6 @@ class Config(BaseConfig):
         return [
             "test/integration_tests/with_db/deployment/test_deploy_cli.py",
             "test/integration_tests/with_db/test_upload_model.py",
-            "test/integration_tests/with_db/deployment/test_install_default_models.py",
             "test/integration_tests/with_db/udfs/test_model_downloader_udf_script.py",
             "test/integration_tests/with_db/udfs/test_prediction_with_downloader_udf.py",
             "test/integration_tests/with_db/udfs/test_delete_model.py",
